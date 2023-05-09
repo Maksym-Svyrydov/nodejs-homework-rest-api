8d010087deeaaef82c9bda87bcaf8a61efec61c6
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2023-05-09 10:14:01 +0300

    fixed err messages

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -22,12 +22,12 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-router.get('/:contactId', async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const { contactId } = req.params;
     const result = await contacts.getContactById(contactId);
     if (!result) {
-      throw HttpError(404, 'There is no contact with this id on the server');
+      throw HttpError(404, 'Not found');
     }
     res.json(result);
   } catch (error) {
@@ -39,7 +39,7 @@ router.post('/', async (req, res, next) => {
   try {
     const { error } = addSchema.validate(req.body);
     if (error) {
-      throw HttpError(400, error.message);
+      throw HttpError(400, 'missing required name field');
     }
     const result = await contacts.addContact(req.body);
     res.status(201).json(result);
@@ -48,7 +48,7 @@ router.post('/', async (req, res, next) => {
   }
 });
 
-router.put('/:contactId', async (req, res, next) => {
+router.put('/:id', async (req, res, next) => {
   try {
     const { error } = addSchema.validate(req.body);
     if (error) {
@@ -57,7 +57,7 @@ router.put('/:contactId', async (req, res, next) => {
     const { contactId } = req.params;
     const result = await contacts.updateContact(contactId, req.body);
     if (!result) {
-      throw HttpError(404, 'There is no contact with this id on the server');
+      throw HttpError(404, 'Not found');
     }
     res.json(result);
   } catch (error) {
@@ -69,9 +69,12 @@ router.delete('/:contactId', async (req, res, next) => {
     const { contactId } = req.params;
     const result = await contacts.removeContact(contactId);
     if (!result) {
-      throw HttpError(404, 'There is no contact with this id on the server');
+      throw HttpError(404, 'Not found');
     }
-    res.json({ result, message: `Remove contacts by id:${contactId} success` });
+    res.json({
+      result,
+      message: `Contact deleted by id:${contactId} `,
+    });
   } catch (error) {
     next(error);
   }
