ba8eaa7372b4d2912948000d52046c08d8d4ec11
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2023-05-12 00:50:42 +0300

    fixed connect

--- server.js
+++ server.js
@@ -4,15 +4,14 @@ const DB_HOST =
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 
-mongoose.connect(DB_HOST).then(() => {
-  app.listen(3000, () => {
-    console.log('Server running. Use our API on port: 3000');
-  });
-  console.log('Database connection success').catch((error) => {
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server running. Use our API on port: 3000');
+    });
+  })
+  .catch((error) => {
     console.log(error.message);
+    process.exit(1);
   });
-});
-app.listen(3000, () => {
-  console.log('Server running. Use our API on port: 3000');
-  process.exit(1);
-});
