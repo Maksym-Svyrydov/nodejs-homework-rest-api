99e10b078ad3b4a640f1c28ed9e43ea3c527e7ec
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2023-05-12 00:40:45 +0300

    init project

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const app = require('./app');
+const DB_HOST =
+  'mongodb+srv://Maksym:assc2012@cluster0.xhngpck.mongodb.net/db-constacts?retryWrites=true&w=majority';
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
 
+mongoose.connect(DB_HOST).then(() => {
+  app.listen(3000, () => {
+    console.log('Server running. Use our API on port: 3000');
+  });
+  console.log('Database connection success').catch((error) => {
+    console.log(error.message);
+  });
+});
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log('Server running. Use our API on port: 3000');
+  process.exit(1);
+});
