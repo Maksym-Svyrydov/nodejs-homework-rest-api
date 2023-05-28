296a286bbdda74063d1c4fb5ff9ed4793bede6ca
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2023-05-28 16:35:10 +0300

    verify done

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,7 +44,7 @@ const verifyEmail = async (req, res) => {
   const { verificationCode } = req.params;
   const user = await User.findOne({ verificationCode });
   if (!user) {
-    throw HttpError(401);
+    throw HttpError(404, 'User not found');
   }
   await User.findByIdAndUpdate(user._id, {
     verify: true,
